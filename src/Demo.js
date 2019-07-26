8b68315ef88abf7cbc684f51d3ea10cc36b9918a
Biswajit Charan <21034383+charanbiswajit123@users.noreply.github.com>
2019-07-26 07:31:45 +0530

    Add files via upload

---
+++ src/Demo.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import './Demo.css';
+
+class Demo extends Component{
+	render(){
+		return <div className="maindiv_style">
+		<h1> Hello {this.props.name} </h1>
+		<p> Welcome to React World </p>
+		</div>
+	}
+}
+export default Demo;
