1391b32be39fa1fca3d8577fe387f67f336a0aa0
biswajit <biswajit@teknotrait.com>
2019-08-12 18:45:04 +0530

    Updated Code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
-import Avatar from "./Avatar.js"
-
-function App() {
-  return (
-    <div className="App">
-     <Avatar name="Biswa"></Avatar>
-     <Avatar name="Charan"></Avatar>
-     <Avatar></Avatar>
-     <Avatar></Avatar>
-    </div>
-  );
-}
-
+import Avatar from './Avatar';
+import Counter from './Counter';
+import logoimg from './assest/img/logo.png';
+class App extends Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+			return (
+			<div>
+			<img className="app-logo" alt="logo" src={logoimg}/>
+			<div className="app-container">
+				<h1>Welcome to Code</h1>
+				<br/><br/>
+				<Avatar />
+			</div>
+			</div>
+			)
+	}
+  /*
+	render() {
+		return (
+			<div className="bodydiv">
+			<div className="titlediv">
+			<img className="logo" alt="logo" src={"logo.png"}/>
+			<p>Don't do you...Do Nu.</p>
+			</div>
+			<div className="buttondiv">
+        <SplashButtonOne />
+			  <SplashButtonTwo />
+			</div>
+			<Avatar />
+			</div>
+		)
+	} */
+} 
 export default App;
--- src/Avatar.js
+++ src/Avatar.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Counter from './Counter';
 import './Avatar.css';
-import img from './assest/img/image.jpg';
-
-const Avatar = (props) => {
-	return (<div className="avatar_style">
-		<img src={img} alt="profile" />
-		<h1> {props.name} </h1>
-		<p> Web Developer </p>
-		</div>);
+const divStyle = {
+	marginBottom: '50px'
+};
+class Avatar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { statedata: [] };
 	}
+	componentDidMount() {
+		fetch('https://randomuser.me/api/?results=12')
+		  .then(results => {
+			 return results.json();
+		  })
+		  .then(data => {
+		  let details = data.results.map((item, index) => {
+		  return (
+			<div key={index} className="col-sm-4" style={divStyle}>
+				<div className="avatar-style">
+					<img src={item.picture.large} />
+					<h4 className="title">Name : {item.name.title}. {item.name.first} {item.name.last}</h4>
+					<p>Gender : {item.gender}</p>
+					<p>Date of Birth : {item.dob.date}</p>
+					<p>Age : {item.dob.age}</p>
+					<p>Email : {item.email}</p>
+					<p>Phone : {item.phone}</p>
+					<p>Location : {item.location.city}</p>
+					Add to Cart : <Counter />
+				</div>
+			</div>
+		  )
+		  })
+		  this.setState({statedata: details});
+		  //console.log("state", this.state.pictures);
+		  })
+	}
+	  
+	render() {
+		  return (
+			<div className="container">
+				<div className="row">					
+					{this.state.statedata}					
+				</div>
+			</div>
+		  )
+	}
+} 
 export default Avatar;
